4bc71599e4c7dc1f1a406241fa9dac77de3792e1
Leandro <leandrovitalp@gmail.com>
2024-08-23 12:31:36 -0300

    add files

---
+++ nextjs-dashboard/app/ui/faturas/status.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+
+export default function InvoiceStatus({ status }: { status: string }) {
+  return (
+    <span
+      className={clsx(
+        'inline-flex items-center rounded-full px-2 py-1 text-sm',
+        {
+          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-green-500 text-white': status === 'paid',
+          'bg-red-500 text-white': status === 'overdue', // Adicione outras condições conforme necessário
+        }
+      )}
+    >
+      {/* Conteúdo da span */}
+      {status}
+    </span>
+  );
+}
+    
